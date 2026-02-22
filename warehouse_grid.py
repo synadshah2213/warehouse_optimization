2f5dc4dfd894be5ae3f444fa60266d18c9b716e9
Syna Shah <synadshah2230gmail.com@Synas-MacBook-Pro.local>
2026-02-22 17:11:19 +0530

    added objects to grid

--- warehouse_grid.py
+++ warehouse_grid.py
@@ -4,10 +4,15 @@ for i in range(10):          # do this 10 times
     row = [0] * 10           # create one row
     grid.append(row)         # add that row to the grid
 #Placeing the objects in the grid
-grid[0][3] = 1
-grid[6][7] = -1
-grid[8][2] = 2
-grid[4][9] = 3
+grid[0][0] = 3
+grid[2][3] = 1
+grid[4][5] = 2
+grid[6][2] = 2
+grid[8][8] = 2
+grid[1][1] = -1
+grid[3][4] = -1
+grid[5][7] = -1
+grid[7][3] = -1
 
 for row in grid:
     print(row)
